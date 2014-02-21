3b33b378fab00246b56e24233e17ac9849737675
Ferdinand Torggler <ferdinand.torggler@gmail.com>
2014-02-21 23:05:43 +0100

    adds support for multiple game rooms

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var
   jade      = require('jade'),
   app       = express(),
 
-  client1,
-  client2;
+  room,
+  rooms = {};
 
 
 // setting up express
@@ -20,178 +20,35 @@ app.get('/', function(req, res) {
     res.render('index.jade');
 });
 
+app.get('/room/:room', function (req, res) {
+    room = req.params.room;
+    res.render('index.jade');
+});
+
 
 // setting up socket.io
 
 var io = socketIO.listen(app.listen(process.env.PORT || 3000));
 io.set('log level', 1);
 
+var Game = require('./lib/game')(io);
 
-var
-  WIDTH           = 100,
-  HEIGHT          = 100,
-
-  BALL_SIZE       = 2,
-  BALL_START    = { x: WIDTH/2 - BALL_SIZE/2, y: HEIGHT/2 - BALL_SIZE/2 },
-  HANDLE_HEIGHT   = 10,
-  HANDLE_WIDTH    = 2,
-  HANDLE_STEP     = 2,
-
-  SPEED           = .3,
-  ACCELERATION    = .1,
-
-  ball            = { x: 0, y: 0 },
-  offset          = { x: 0, y: 0 },
-
-  player1         = { y: HEIGHT/2 - HANDLE_HEIGHT/2 },
-  player2         = { y: HEIGHT/2 - HANDLE_HEIGHT/2 },
-
-  score           = { player1: 0, player2: 0 },
-
-  gameloop;
-
-
-var newBall = function () {
-    offset.x = SPEED;
-    offset.y = SPEED;
-    if (Math.random() > 0.5) offset.x = -offset.x;
-    if (Math.random() > 0.5) offset.y = -offset.y;
-    ball.x = BALL_START.x;
-    ball.y = BALL_START.y;
+var getGameOfPlayer = function (player) {
+    var room;
+    for(var k in io.sockets.manager.roomClients[player.id])
+        if (k !== '') room = k;
+    return rooms[room.replace('/', '')];
 };
-
-var resetGame = function () {
-    clearInterval(gameloop);
-    gameloop = undefined;
-    client1 = undefined;
-    client2 = undefined;
-    player1 = {y: HEIGHT/2 - HANDLE_HEIGHT/2};
-    player2 = {y: HEIGHT/2 - HANDLE_HEIGHT/2};
-    score = {player1: 0, player2: 0};
-    newBall()
-};
-
-var sendHandlePositions = function () {
-    io.sockets.emit('handle positions', {player1: player1.y, player2: player2.y});
-};
-
-var bounceBackOnWallCollision = function () {
-    if (ball.y <= 0 || ball.y + BALL_SIZE >= HEIGHT) {
-        offset.y = -offset.y;
-        io.sockets.emit('wall touched');
-    }
-};
-
-var updateScoreIfScored = function () {
-    // when the ball reaches the left end, player 2 scores
-    if (ball.x <= 0) {
-        score.player2 += 1;
-        io.sockets.emit('score', score);
-        newBall();
-    }
-
-    // when the ball reaches the right end, player 1 scores
-    if (ball.x + BALL_SIZE >= WIDTH) {
-        score.player1 += 1;
-        io.sockets.emit('score', score);
-        newBall();
-    }
-};
-
-var bounceBackOnHit = function () {
-
-    var player1Touches = ( (ball.x <= HANDLE_WIDTH)
-                        && (ball.y + BALL_SIZE >= player1.y)
-                        && (ball.y <= player1.y + HANDLE_HEIGHT));
-
-    var player2Touches = ( (ball.x + BALL_SIZE >= (WIDTH - HANDLE_WIDTH))
-                        && (ball.y + BALL_SIZE >= player2.y)
-                        && (ball.y <= player2.y + HANDLE_HEIGHT));
-
-
-    if (player1Touches || player2Touches) {
-
-        // accelerate ball in random direction
-        if (offset.x < 2) {
-            offset.x += (offset.x < 0) ? -ACCELERATION : ACCELERATION;
-            offset.y = SPEED * 2 * Math.random(); // arithmetic mean is SPEED
-        }
-
-        offset.x = -offset.x;
-        io.sockets.emit('handle touched');
-    }
-
-};
-
 
 io.sockets.on('connection', function (socket) {
 
-    var startData = function (player) {
-        return { handleHeight: HANDLE_HEIGHT,
-                 handleWidth: HANDLE_WIDTH,
-                 ballSize: BALL_SIZE,
-                 player: player
-               };
-    };
-
-    if (client1 === undefined) {
-        client1 = socket;
-        socket.emit('game data', startData(1));
-        sendHandlePositions();
-    } else if (client2 === undefined) {
-        client2 = socket;
-        socket.emit('game data', startData(2));
-        sendHandlePositions();
-    } else {
-        resetGame();
+    if (room) {
+        if (!rooms[room]) rooms[room] = new Game(room);
+        rooms[room].addPlayer(socket);
     }
 
-    var moveHandle = function (player, amount) {
-        setHandle(player, player.y + amount);
-    };
-
-    var setHandle = function (player, y) {
-        player.y = y;
-
-        if (player.y < 0) player.y = 0;
-        if (player.y + HANDLE_HEIGHT > HEIGHT) player.y = HEIGHT - HANDLE_HEIGHT;
-        sendHandlePositions();
-    };
-
-    socket.on('move handle up', function (player) {
-        moveHandle(((player === 1) ? player1 : player2), -HANDLE_STEP);
-    });
-
-    socket.on('move handle down', function (player) {
-        moveHandle(((player === 1) ? player1 : player2), HANDLE_STEP);
-    });
-
-    socket.on('move handle', function (data) {
-        setHandle(((data.player === 1) ? player1 : player2), data.y);
-    });
-
-    var tick = function () {
-
-        ball.x += offset.x;
-        ball.y += offset.y;
-
-        bounceBackOnHit();
-        bounceBackOnWallCollision();
-        updateScoreIfScored();
-
-        io.sockets.emit('ball move', { x: ball.x, y: ball.y });
-    };
-
-    newBall();
-
-    if (!gameloop && client1 && client2) {
-        io.sockets.emit('start game');
-        newBall();
-        gameloop = setInterval(tick, 1000/60);
-    }
-
-    socket.on('disconnect', function () {
-        resetGame();
-    });
+    socket.on('move handle up', function (data) { getGameOfPlayer(socket).moveHandleUp(data); });
+    socket.on('move handle down', function (data) { getGameOfPlayer(socket).moveHandleDown(data); });
+    socket.on('move handle', function (data) { getGameOfPlayer(socket).moveHandleToPosition(data); });
 
 });
